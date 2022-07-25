52322b724979a85c4073bf7f30f56a99a604c1ea
mpham-12 <martinp0512@gmail.com>
2022-07-25 15:46:13 -0400

    need to fix bug

---
+++ longest-common-prefix.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+
+//UNFINISHED
+var longestCommonPrefix = function(strs) {
+  let prefix = strs[0];
+
+  for (let i = 1; i < strs.length; i++) {
+    while (strs[1].indexOf(prefix) !== 0) {
+      prefix = prefix.substring(0, prefix.length-1)
+    }
+  }
+  return prefix
+};
+
+console.log(longestCommonPrefix(["dog", "racecar", "car"]));
+console.log(longestCommonPrefix(["flower", "flow", "flight"]));
