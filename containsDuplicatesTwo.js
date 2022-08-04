6f71a354cac1e109ee26473ca8e686c5d7863c93
mpham-12 <martinp0512@gmail.com>
2022-08-03 22:11:53 -0400

    incomplete

--- containsDuplicatesTwo.js
+++ containsDuplicatesTwo.js
@@ -3,6 +3,23 @@
  * @param {number} k
  * @return {boolean}
  */
- var containsNearbyDuplicate = function(nums, k) {
-    
+
+//INCOMPLETE
+var containsNearbyDuplicate = function(nums, k) {
+const duplicates = nums.filter((num, index)=>{
+  return nums.indexOf(num) !== index
+})
+
+
+for (let i=0; i<nums.length;i++){
+  if (duplicates.includes(num<=k)){
+    return true;
+  } else {
+    return false;
+  }
+}
 };
+
+console.log(containsNearbyDuplicate([1, 2, 3, 1], 3));
+console.log(containsNearbyDuplicate([1, 0, 1, 1], 1));
+console.log(containsNearbyDuplicate([1, 2, 3, 1, 2, 3], 2));
