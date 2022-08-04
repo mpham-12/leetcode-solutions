82c67ef78e0229dc2f2e835515913af9e726bf77
mpham-12 <martinp0512@gmail.com>
2022-08-03 21:42:02 -0400

    solved

--- containsDuplicates.js
+++ containsDuplicates.js
@@ -2,6 +2,15 @@
  * @param {number[]} nums
  * @return {boolean}
  */
- var containsDuplicate = function(nums) {
-    
+var containsDuplicate = function(nums) {
+
+  const filteredNums = [...new Set(nums)];
+  if (nums.length !== filteredNums.length) {
+    return true
+  }
+  return false;
 };
+
+console.log(containsDuplicate([1, 2, 3, 1]));
+console.log(containsDuplicate([1, 2, 3, 4]));
+console.log(containsDuplicate([1, 1, 1, 3, 3, 4, 3, 2, 4, 2]));
