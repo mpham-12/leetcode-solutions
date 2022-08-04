6de7e34147a2356b0503ab68da980e6bd01c3bda
mpham-12 <martinp0512@gmail.com>
2022-08-03 21:25:50 -0400

    solved

--- firstLetterToAppearTwice.js
+++ firstLetterToAppearTwice.js
@@ -2,6 +2,15 @@
  * @param {string} s
  * @return {character}
  */
- var repeatedCharacter = function(s) {
-    
+var repeatedCharacter = function(s) {
+  const arr = s.split('');
+  const duplicate = arr.filter((char, index) => {
+    return arr.indexOf(char) !== index
+  })
+  return duplicate[0].toString();
+
 };
+
+console.log(repeatedCharacter("abccbaacz"))
+console.log(repeatedCharacter("abcdd"))
+console.log(repeatedCharacter("abca"))
