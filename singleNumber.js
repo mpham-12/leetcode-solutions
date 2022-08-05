cc3e2ee0bb4de0c152b385a538cff110af213c93
mpham-12 <martinp0512@gmail.com>
2022-08-05 13:05:26 -0400

    initial commit

--- singleNumber.js
+++ singleNumber.js
@@ -2,6 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
- var singleNumber = function(nums) {
-    
+var singleNumber = function(nums) {
+  const output = nums.filter((num, index) => {
+    return nums.indexOf(num) === nums.lastIndexOf(num)
+  });
+  return output
 };
+
+console.log(singleNumber([2, 2, 1]));
+console.log(singleNumber([4, 1, 2, 1, 2]));
+console.log(singleNumber([1]));
