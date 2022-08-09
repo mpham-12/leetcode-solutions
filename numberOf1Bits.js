154f99b9662ad3647177e4b9b60e95c4af2d6bc0
mpham-12 <martinp0512@gmail.com>
2022-08-09 12:41:32 -0400

    initial commit

--- numberOf1Bits.js
+++ numberOf1Bits.js
@@ -2,6 +2,16 @@
  * @param {number} n - a positive integer
  * @return {number}
  */
+//INCOMPLETE
  var hammingWeight = function(n) {
-    
+    const obj={};
+    const arr= Array.from(String(n), Number);
+    console.log(arr)
+    // for (bit of arr){
+    //   obj[bit]=(obj[bit]||0)+1;
+    // }
+// console.log(obj)
 };
+
+console.log(hammingWeight(000001))
+// console.log(hammingWeight(11111111111111111111111111111101))
